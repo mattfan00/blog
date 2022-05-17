0c532813dade6405ecf42954a35a4fd8aca7d2cf
Matthew Fan <kungfumo@gmail.com>
2022-05-16 22:00:12 -0400

    modified npm start

--- index.ts
+++ index.ts
@@ -2,14 +2,10 @@ import fs from "fs"
 import path from "path"
 import fm from "front-matter"
 import MarkdownIt from "markdown-it"
-import Handlebars, { template } from "handlebars"
+import Handlebars from "handlebars"
 import * as constants from "./constants"
 
 const md = new MarkdownIt("commonmark")
-
-// const postTemplate = Handlebars.compile(fs.readFileSync("./template/post.html", "utf8"))
-// const directoryTemplate = Handlebars.compile(fs.readFileSync("./template/directory.html", "utf8"))
-// const aboutTemplate = Handlebars.compile(fs.readFileSync("./template/about.html", "utf8"))
 
 Handlebars.registerHelper('slicePath', function (str) {
   return str.slice(0, -3)
@@ -79,60 +75,3 @@ const convertContentToPublic = (from: string, to: string) => {
 }
 
 convertContentToPublic(constants.PATH_CONTENT, constants.PATH_PUBLIC)
-
-
-// // create posts
-// const posts = fs.readdirSync(constants.PATH_CONTENT)
-//   .filter(postPath => postPath !== "about.md")
-//   .map(postPath => {
-//     const rawText = fs.readFileSync(`${constants.PATH_CONTENT}/${postPath}`, "utf8")
-//     const content = fm<ContentAttributes>(rawText)
-
-//     return {
-//       ...content,
-//       body: md.render(content.body),
-//       path: postPath
-//     }
-//   })
-//   .map(post => {
-//     const postDir = `${constants.PATH_PUBLIC}/${post.path.slice(0, -3)}`
-//     if (!fs.existsSync(postDir)) {
-//       console.log(post.attributes.title + " doesnt exist")
-//       fs.mkdirSync(postDir)
-//     }
-//     const postHTML = postTemplate(post)
-//     fs.writeFileSync(`${postDir}/index.html`, postHTML)
-
-//     return post
-//   })
-
-// // create home directory
-// const sortedPosts = posts.sort((a, b) => {
-//   return new Date(b.attributes.date).getTime() - new Date(a.attributes.date).getTime()
-// })
-// const directoryHTML = directoryTemplate({posts: sortedPosts})
-// fs.writeFileSync(`${constants.PATH_PUBLIC}/index.html`, directoryHTML)
-
-// // create about page
-// const aboutPosts = fs.readdirSync(constants.PATH_CONTENT)
-//   .filter(postPath => postPath === "about.md")
-//   .map(postPath => {
-//     const rawText = fs.readFileSync(`${constants.PATH_CONTENT}/${postPath}`, "utf8")
-//     const content = fm<ContentAttributes>(rawText)
-
-//     return {
-//       ...content,
-//       body: md.render(content.body),
-//       path: postPath
-//     }
-//   })
-
-// const aboutPost = aboutPosts[0]
-
-// const aboutDir = `${constants.PATH_PUBLIC}/about`;
-// if (!fs.existsSync(aboutDir)) {
-//   fs.mkdirSync(aboutDir)
-// }
-
-// const aboutHTML = aboutTemplate(aboutPost)
-// fs.writeFileSync(`${constants.PATH_PUBLIC}/about/index.html`, aboutHTML)
